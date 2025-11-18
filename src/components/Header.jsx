5314241565853d4e9cc3d4cfd3d93e118be0100c
Abdullah Al Faiyaz <abdullah-al.faiyaz@stud.uni-bamberg.de>
2025-11-18 22:54:35 +0100

    minor adjustment

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -10,8 +10,7 @@ const Header = () => {
       <div className="relative">
         <FaShoppingCart className="text-2xl text-gray-700" />
         {itemCount > 0 && (
-          <span className="absolute -top-2 -right-2 bg-red-500 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
-            {/* {itemCount} */}
+          <span className="absolute -top-2 -right-2 bg-red-600 text-white text-xs w-5 h-5 flex items-center justify-center rounded-full">
             {itemCount}
           </span>
         )}
